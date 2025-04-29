e01af4641284268201ccda469a221fa99d65c8cd
Chelle007 <mc.chellechan@gmail.com>
2025-04-29 18:44:32 +0800

    add gradient and margin and text-size

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,9 +7,9 @@ export default function HeroSection() {
             <div className="absolute inset-0 z-0 bg-stars-pattern bg-repeat opacity-50"></div>
 
             {/* Club Name & Tagline */}
-            <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
-                <h1 className="font-saint-pauline text-6xl md:text-9xl font-extralight text-[#4f3e37] tracking-wide">Sim Esprimere</h1>
-                <p className="font-dream-avenue text-1xl md:text-4xl text-[#4f3e37]">Time to Take Centre Stage</p>
+            <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 bg-gradient-to-b from-[var(--color-cream-1)] to-[var(--color-brown-3)]">
+                <h1 className="font-saint-pauline text-6xl md:text-[10rem] font-extralight text-[var(--color-brown-1)] tracking-wide">Sim Esprimere</h1>
+                <p className="font-dream-avenue text-1xl md:text-5xl text-[var(--color-brown-1)] -mt-4">Time to Take Centre Stage</p>
             </div>
 
             {/* Cloud Bottom Separator */}
